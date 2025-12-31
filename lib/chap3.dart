85be49dc85d34cb04c37c2f347086e68f99f1cef
nipunmnndhr10 <nipun16man@gmail.com>
2025-12-31 15:37:43 +0545

    SafeArea

--- lib/chap3.dart
+++ lib/chap3.dart
@@ -23,51 +23,53 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Scaffold AppBar"),
-          centerTitle: true,
-          // leading: Icon(Icons.home),
-          // actions: [Text("Exit Action"), Icon(Icons.exit_to_app)],
-          // backgroundColor: Colors.teal,
-        ),
-        drawer: Drawer(
-          child: Column(
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Scaffold AppBar"),
+            centerTitle: true,
+            // leading: Icon(Icons.home),
+            // actions: [Text("Exit Action"), Icon(Icons.exit_to_app)],
+            // backgroundColor: Colors.teal,
+          ),
+          drawer: Drawer(
+            child: Column(
+              children: [
+                // DrawerHeader(child: Text("Drawer Header in col")),
+                ListTile(title: Text("Logout")),
+              ],
+            ),
+          ),
+          bottomNavigationBar: NavigationBar(
+            //! needs at least two destination to work
+            destinations: [
+              NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+              NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+            ],
+            // below func will return the value that we've clicked
+            onDestinationSelected: (int value) {
+              print(value);
+            },
+            selectedIndex: 1,
+          ),
+          floatingActionButton: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              DrawerHeader(child: Text("Drawer Header in col")),
-              ListTile(title: Text("Logout")),
+              FloatingActionButton(
+                onPressed: () {
+                  print("Floating action button2 pressed");
+                },
+                child: Icon(Icons.add),
+              ),
+              SizedBox(height: 10),
+              FloatingActionButton(
+                onPressed: () {
+                  print("Floating action button1 pressed");
+                },
+                child: Icon(Icons.add),
+              ),
             ],
           ),
-        ),
-        bottomNavigationBar: NavigationBar(
-          //! needs at least two destination to work
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-            NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
-          ],
-          // below func will return the value that we've clicked
-          onDestinationSelected: (int value) {
-            print(value);
-          },
-          selectedIndex: 1,
-        ),
-        floatingActionButton: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                print("Floating action button2 pressed");
-              },
-              child: Icon(Icons.add),
-            ),
-            SizedBox(height: 10),
-            FloatingActionButton(
-              onPressed: () {
-                print("Floating action button1 pressed");
-              },
-              child: Icon(Icons.add),
-            ),
-          ],
         ),
       ),
     );
